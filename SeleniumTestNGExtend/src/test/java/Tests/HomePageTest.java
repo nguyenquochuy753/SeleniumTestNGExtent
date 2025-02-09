f44ef06ee678d445e803d47f401dbb198779a8b6
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2025-02-09 19:11:41 +0700

    upadted

--- SeleniumTestNGExtend/src/test/java/Tests/HomePageTest.java
+++ SeleniumTestNGExtend/src/test/java/Tests/HomePageTest.java
@@ -4,12 +4,9 @@ import Pages.HomePage;
 import org.testng.annotations.Test;
 
 public class HomePageTest extends BaseTest{
-    @Test
-    public void validateNavigationToOnlineProducts() throws InterruptedException {
-        HomePage.click_hamburger_menu();
-        HomePage.click_onlineProducts_link();
-    }
-
-
-
+//    @Test
+//    public void validateNavigationToOnlineProducts() throws InterruptedException {
+//        HomePage.click_hamburger_menu();
+//        HomePage.click_onlineProducts_link();
+//    }
 }
